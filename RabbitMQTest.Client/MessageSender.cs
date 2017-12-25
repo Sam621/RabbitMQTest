def2f63497df7b1332074d414a2dd2069386afa3
sanjaya621@gmail.com <sanjaya621@gmail.com>
2017-12-25 21:36:26 +1100

    Adding Client again

---
+++ RabbitMQTest.Client/MessageSender.cs
@@ -0,0 +1,88 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQTest.Client
+{
+    public class MessageSender: IDisposable
+    {
+        private const string HostName = "localhost";
+        private const string UserName = "guest";
+        private const string Passowrd = "guest";
+        private static string exchangeName = "";
+        private static string queueName = "MyQuece";
+        private static bool IsDurable = true;
+
+
+        private const string VirtualHost = "";
+        private int port = 0;
+
+        private ConnectionFactory connectionFactory;
+        private IConnection connection;
+        private IModel model;
+
+        public MessageSender()
+        {
+            DisplaySettigs();
+            SetupRabbitMq();
+        }
+
+        private void SetupRabbitMq()
+        {
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                Password = Passowrd,
+                UserName = UserName,
+                HostName = HostName
+            };
+
+            if (!string.IsNullOrEmpty(VirtualHost))
+            {
+                connectionFactory.VirtualHost = VirtualHost;
+            }
+
+            if(port > 0)
+            {
+                connectionFactory.Port = port;
+            }
+
+            var connection = connectionFactory.CreateConnection();
+            model = connection.CreateModel();
+
+            //model.QueueDeclare(queueName, true, false, false, null);
+
+            //model.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+
+
+            //model.QueueBind(queueName, exchangeName, "TestKey");
+        }
+
+
+        public void Send(string message)
+        {
+            var properties = model.CreateBasicProperties();
+            properties.SetPersistent(false);
+
+            byte[] messageBuffer = Encoding.Default.GetBytes(message);
+
+            // send message
+            model.BasicPublish(exchangeName, queueName, properties, messageBuffer);
+
+
+        }
+
+       
+        private void DisplaySettigs()
+        {
+            Console.WriteLine("Host: {0}", HostName);
+        }
+
+        void IDisposable.Dispose()
+        {
+            
+        }
+    }
+}
---
+++ RabbitMQTest.Client/Program.cs
@@ -0,0 +1,96 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQTest.Client
+{
+    class Program
+    {
+        private const string HostName = "localhost";
+        private const string UserName = "guest";
+        private const string Passowrd = "guest";
+        private static string exchangeName= "MyExchange";
+        private static string queueName = "MyQuece";
+
+        static void Main(string[] args)
+        {
+            //CreateQueue(); ;
+            //BasicPublish();
+            SimpleOneWayMessage();
+        }
+
+        private static void SimpleOneWayMessage()
+        {
+            Console.WriteLine("");
+
+            var messageCount = 0;
+            var sender = new MessageSender();
+
+            Console.WriteLine("Type message and press enter to sender message");
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                if(key.Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+
+                if(key.Key == ConsoleKey.Enter)
+                {
+                    var message = string.Format("Message: {0}", messageCount);
+                    Console.WriteLine(string.Format("Sending - {0}", message));
+                    sender.Send(message);
+                    messageCount++;
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static void CreateQueue()
+        {
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                Password = Passowrd,
+                UserName = UserName,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            model.QueueDeclare(queueName, true, false, false, null);
+
+            model.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+
+
+            model.QueueBind(queueName, exchangeName, "TestKey");
+        }
+
+        static void BasicPublish()
+        {
+            var connectionFactory = new RabbitMQ.Client.ConnectionFactory()
+            {
+                Password = Passowrd,
+                UserName = UserName,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            var properties = model.CreateBasicProperties();
+            properties.SetPersistent(false);
+
+            byte[] messageBuffer = Encoding.Default.GetBytes("My simple message");
+
+            // send message
+            model.BasicPublish( "", queueName, properties, messageBuffer);
+            
+
+            Console.WriteLine("Message sent");
+           
+        }
+    }
+}
+
